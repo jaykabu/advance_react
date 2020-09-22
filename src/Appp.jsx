f663d364d2d7bbd6e23fa0c27aaded73c0d62fce
JayKabutarwala <59605424+jaykabu@users.noreply.github.com>
2020-09-22 18:05:38 +0530

    ok

---
+++ src/Appp.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+// USE STATE IN HOOK
+
+const Appp = () => {
+
+    const state = useState()
+
+    const [count, setCount] = useState(0);
+
+    const IncNub = () => {
+        setCount(count + 1)
+    }
+
+    return (
+        <>
+            <h1>{count}</h1>
+            <button onClick={IncNub}>Click Me</button>
+        </>
+    )
+};
+
+export default Appp;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './Appp';
+import './index.css';
+import Appp from "./Appp";
 
 
 ReactDOM.render(
   <>
-    <Appp />
+      {/*<div>*/}
+        <Appp />
+      {/*</div>*/}
   </>,
   document.getElementById('root')
 );
