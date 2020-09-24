e9be62e099e3333ca5a8bfec959e4ac2c9c2fec2
JayKabutarwala <59605424+jaykabu@users.noreply.github.com>
2020-09-24 11:53:44 +0530

    default

--- src/Appp.jsx
+++ src/Appp.jsx
@@ -22,27 +22,93 @@
 // export default Appp;
 
 
+// import React, {useState} from "react";
+//
+// const Appp = () => {
+//
+//     let Time = new Date().toLocaleTimeString()
+//
+//     const [ctime, setCtime] = useState(Time)
+//
+//
+//     const Ctime = () => {
+//         let newTime = new Date().toLocaleTimeString()
+//         setCtime(newTime);
+//     }
+//
+//     return (
+//         <>
+//             <h1>{ctime}</h1>
+//             <button onClick={Ctime}> Get Time</button>
+//         </>
+//     )
+// }
+//
+//
+// export default Appp;
+
+
+// import React, {useState} from "react";
+//
+// const Appp = () => {
+//
+//     const purple = "8e44ad";
+//     const [bg, setBg] = useState(purple)
+//     const [name , setName] = useState("CLICK ME")
+//
+//     const bgChange = () => {
+//         const cRed = "#FE2E2E";
+//          setBg(cRed)
+//         setName('Ouch 😲');
+//     }
+//
+//     const cColor = () => {
+//         let cBlue = "black";
+//         setBg(cBlue)
+//         setName('angry 😠')
+//
+//     }
+//
+//     return (
+//         <>
+//             <div style={{backgroundColor: bg}}>
+//                 <button onClick={bgChange}  onDoubleClick={cColor} >{name}  </button>
+//             </div>
+//         </>
+//     )
+// };
+//
+// export default Appp;
+//
+//
+
+
 import React, {useState} from "react";
 
 const Appp = () => {
 
-    let Time = new Date().toLocaleTimeString()
+    const [name , setName] = useState('')
+    const [fullName , setFullName] = useState('')
 
-    const [ctime, setCtime] = useState(Time)
 
+    const inputEvent = (event)=>{
+       // console.log(event.target.value)
+       setName(event.target.value)
+    }
 
-    const Ctime = () => {
-        let newTime = new Date().toLocaleTimeString()
-        setCtime(newTime);
+    const inputSumit = ()=>{
+        setFullName(name)
     }
 
     return (
         <>
-            <h1>{ctime}</h1>
-            <button onClick={Ctime}> Get Time</button>
+            <div>
+                <h1>Hello {fullName}</h1>
+                <input type={'text'}  placeholder={'Enter Your Name'} onChange={inputEvent} value={name}/>
+                <button onClick={inputSumit}>Submit</button>
+            </div>
         </>
     )
-}
-
+};
 
 export default Appp;
