15091f01a58f03ac3377c823489bc572ed357a72
JayKabutarwala <59605424+jaykabu@users.noreply.github.com>
2020-10-05 17:43:01 +0530

    pokemon api call

---
+++ src/Apicall.jsx
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+
+const Apicall = () => {
+
+    const [num, setNum] = useState()
+    const [name, setName] = useState();
+    const [moves, setMoves] = useState();
+
+
+    useEffect(() => {
+        async function getData() {
+            const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+            console.log(res.data)
+            setName(res.data.name);
+            setMoves(res.data.moves.length)
+        }
+
+        getData();
+    });
+
+    return (
+        <>
+            <h1>You Choose {num} Value</h1>
+            <h1>My name is {name} </h1>
+            <h1> I have {moves} moves </h1>
+
+            <select value={num} onChange={(event) => {
+                setNum(event.target.value)
+            }}>
+                <option value={'1'}>1</option>
+                <option value={'2'}>2</option>
+                <option value={'3'}>3</option>
+                <option value={'4'}>4</option>
+                <option value={'25'}>25</option>
+            </select>
+        </>
+    )
+}
+
+export default Apicall;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import Appp from "./Appp";
+import Appp from "./Appp";
 import ContextApi from "./ContextApi";
+import Apicall from "./Apicall";
 
 
 ReactDOM.render(
     <>
         {/*<div>*/}
         {/*  <Appp />*/}
-        <ContextApi/>
+        {/*<ContextApi/>*/}
+        <Apicall/>
         {/*</div>*/}
     </>,
     document.getElementById('root')
