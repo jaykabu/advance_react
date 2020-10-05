cc38c0b61583a13fadec0bda240f69780356c0b3
JayKabutarwala <59605424+jaykabu@users.noreply.github.com>
2020-10-05 17:44:02 +0530

    ContextApi

--- src/ContextApi.jsx
+++ src/ContextApi.jsx
@@ -1,22 +1,88 @@
-//CONTEXT API
+// //CONTEXT API
+//
+// import React, {createContext} from 'react';
+// import ComA from './ComA';
+//
+// const FirstName = createContext();
+// const LastName = createContext();
+//
+// const ContextApi = () => {
+//     return (
+//         <>
+//             <FirstName.Provider value={'JaY'}>
+//                 <LastName.Provider value={'Kabu'}>
+//                     <ComA/>
+//                 </LastName.Provider>
+//             </FirstName.Provider>
+//         </>
+//     )
+// };
+//
+// export default ContextApi;
+// export {FirstName, LastName};
 
-import React, {createContext} from 'react';
-import ComA from './ComA';
 
-const FirstName = createContext();
-const LastName = createContext();
+//using useEffect Hook
+
+// import React, {useEffect, useState} from "react";
+//
+// const ContextApi = () => {
+//
+//     const [num , setNum] = useState(0);
+//     const [num1 , setNum1] = useState(0);
+//
+//
+//     useEffect(()=>{
+//         alert('clicked')
+//     }, [num]);
+//
+//     const inputEvent = ()=> {
+//         setNum(num + 1);
+//     }
+//
+//     const inputEvent1 = ()=>{
+//         setNum1(num1 + 1)
+//     }
+//     return (
+//         <>
+//             <h1>{num}</h1>
+//             <button onClick={inputEvent}>Click Me</button>
+//
+//             <br/>
+//
+//             <h1>{num1}</h1>
+//             <button onClick={inputEvent1}>Click Me</button>
+//         </>
+//     )
+// };
+//
+// export default ContextApi;
+
+
+//challenge
+
+import React, {useEffect, useState} from "react";
 
 const ContextApi = () => {
+
+    const [num, setNum] = useState(0)
+
+    useEffect(() => {
+        document.title = `You clicked ${num} times`;
+    })
+
+    const inputEvent = () => {
+        setNum(num + 1)
+    }
     return (
         <>
-            <FirstName.Provider value={'JaY'}>
-                <LastName.Provider value={'Kabu'}>
-                    <ComA/>
-                </LastName.Provider>
-            </FirstName.Provider>
+            <h1> {num} </h1>
+            <button onClick={inputEvent}> Click Me</button>
         </>
     )
 };
 
 export default ContextApi;
-export {FirstName, LastName};
+
+
+
