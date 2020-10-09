9ad4ce4c93a8903d5c400ac79d221f1448788362
JayKabutarwala <59605424+jaykabu@users.noreply.github.com>
2020-10-09 11:25:11 +0530

    Error page create

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,11 +1,26 @@
 //ROUTER
 
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 const Error = () => {
     return (
         <>
-            <h1>Oops ! page not found.</h1>
+
+            <div style={{
+                textAlign: 'center',
+                marginTop: '20%',
+                fontSize: '20px',
+                fontFamily: 'source code pro',
+                color: 'blue'
+            }}
+            >
+                <h1>404 Oops ! page not found.</h1>
+                <p style={{fontSize: '20px', paddingTop: '15px', fontFamily: 'source code pro'}}> Sorry , This page
+                    doesn't exist </p>
+                <NavLink to={'/'}>Go Back</NavLink>
+            </div>
+
         </>
     )
 };
