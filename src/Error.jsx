2aaff4f741fa14918290d57e013e70e3b250bc69
nadeem159 <38816499+nadeem159@users.noreply.github.com>
2020-10-22 14:09:23 +0530

    add some css code

--- src/Error.jsx
+++ src/Error.jsx
@@ -10,13 +10,13 @@ const Error = () => {
             <div style={{
                 textAlign: 'center',
                 marginTop: '20%',
-                fontSize: '20px',
+                fontSize: '21px',
                 fontFamily: 'source code pro',
                 color: 'blue'
             }}
             >
-                <h1>404 Oops ! page not found.</h1>
-                <p style={{fontSize: '20px', paddingTop: '15px', fontFamily: 'source code pro'}}> Sorry , This page
+                <h1>500 Oops ! Server Side Error.</h1>
+                <p style={{fontSize: '20px', paddingTop: '15px'}}> Sorry , This page
                     doesn't exist </p>
                 <NavLink to={'/'}>Go Back</NavLink>
             </div>
